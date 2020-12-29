2b76186aa1df1fc6c8d1331583064e08906dd71e
Fiach Reid <fiach@globaldataconsortium.com>
2020-12-29 18:51:32 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Emgu.CV;
+
+namespace Webcam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Install-Package Emgu.CV 
+            // Install-Package Emgu.CV.runtime.windows
+            var filename = "webcam.jpg";
+            if (args.Length > 0) filename = args[0];
+            using var capture = new VideoCapture(0, VideoCapture.API.DShow); //create a camera capture
+            var image = capture.QueryFrame(); //take a picture
+            image.Save(filename);
+        }
+    }
+}
